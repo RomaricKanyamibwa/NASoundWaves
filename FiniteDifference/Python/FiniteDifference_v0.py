208de4143ef29e6c89d0f953aad391325f0bc717
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2018-07-04 13:42:54 +0900

    Matrix non inversible

---
+++ FiniteDifference/Python/FiniteDifference_v0.py
@@ -0,0 +1,317 @@
+"""
+test  schemas DF et visu
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 15:25:35 2018
+@author: SATGE Lancelot, KANYAMIBWA Romaric
+Resolution of PDE using Finite Difference
+================
+Test of Finite Difference Numerical Schemes on sound waves equations
+"""
+
+#############################################################################
+#  Copyright (Const_c) 2017                                                       #
+#                                                                           #
+#                                                                           #
+#  Distributed under the terms of the GNU General Public License (GPL)      #
+#  either version 3, or (at your option) any later version                  #
+#                                                                           #
+#  http://www.gnu.org/licenses/                                             #
+#############################################################################
+import numpy
+import os
+
+import scipy.sparse as sparse
+import scipy.sparse.linalg
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import random
+
+from pprint import pprint
+
+from sympy.matrices import SparseMatrix
+
+### We want to resolve numericaly the following PDE
+#
+# d_t u1 + 1/2 * d_x1 PH0 = 0
+# d_t PH0 + 5/3 * dj_x1 u1 = 0
+#
+# dx2=0 and dx3=0
+# with u the velocity and P the pressure
+
+## vitesse max km/h
+#v_max = 130
+
+## densite max (nb de voitures/km)
+#u_max = 200
+
+
+#def vitesse(u_val):    
+    #if(0<u_val<u_max):
+        #return -0.65*u_val+130
+    #else:
+        #return 0
+
+#def flux(u_val):    
+    #return u_val*vitesse(u_val)
+
+
+
+# time discretization: 
+# du/dt(t^n) =  
+# 1:  ( u^n - u^n-1 )/dt        (impl) 
+# 2:  ( u^n+1 - u^n )/dt        (expl) 
+t_choice = 2 
+
+# space discretization: 
+# du/dx(x_i) =  
+# 1:  ( u_i - u_i-1 )/dx 
+# 2:  ( u_i+1 - u_i )/dx 
+# 3:  ( u_i+1 - u_i-1 )/2dx
+x_choice = 3
+
+Const_c=-1./2
+Const_d=-5./3
+
+implicite = True
+
+if implicite:
+    dir_name = 'simu_impliciteV0/'
+else:
+    dir_name = 'simu_expliciteV0/'
+
+do_movie = False
+if do_movie:    
+    movie_filename = dir_name+'movie.mp4'
+
+# spatial domain  and  meshing
+X1_min = 0
+X1_max = 1
+Nx1 = 5
+h1 = 1./Nx1
+X1 = numpy.zeros(Nx1)
+
+X2_min = 0
+X2_max = 1
+Nx2 = Nx1
+h2 = 1./Nx2
+X2 = numpy.zeros(Nx2)
+
+X3_min = 0
+X3_max = 1
+Nx3 = Nx1
+h3 = 1./Nx3
+X3 = numpy.zeros(Nx3)
+
+# temporal domain
+Temps_final = 1.#/60
+Nt = 500
+dt = Temps_final * 1./Nt
+dt_over_h1 = dt/h1
+dt_over_h2 = dt/h2
+dt_over_h3 = dt/h3
+
+dt_array=[dt_over_h1,dt_over_h2,dt_over_h3]
+h_array=[h1,h2,h3]
+Nx_array=[Nx1,Nx2,Nx3]
+X_array=[[X1_min,X1_max,X1],[X2_min,X2_max,X2],[X3_min,X3_max,X3]]
+
+# approximate number of images generated
+n_images = 100
+periode_images = int(Nt*1./n_images)
+
+# Pressure and Numerical solution (approx sol)
+PH0 = numpy.zeros(max(Nx1,Nx2,Nx3))
+
+u1 = numpy.zeros(Nx1)
+next_u1 = numpy.zeros(Nx1)     
+
+u2 = numpy.zeros(Nx2)
+next_u2 = numpy.zeros(Nx2)     
+
+u3 = numpy.zeros(Nx3)
+next_u3 = numpy.zeros(Nx3)
+
+u_array=[u1,u2,u3,PH0]
+
+
+# fonction initiale
+
+#At t=0 and x1=xw u=0 and P=0
+#plot_sol(0,0)
+
+def u1_ini(t):
+    return -numpy.sin(t)+(1+t)*numpy.exp(-t)
+
+def u2_ini(x):
+    return 60+20*numpy.sin(2*numpy.pi*x)
+
+def u3_ini(x):
+    return 60+20*numpy.sin(2*numpy.pi*x)
+
+for i in range(0,Nx1):
+    X1[i] = X1_min + i*h1*(X1_max-X1_min)
+    u1[i] = u1_ini(dt)
+
+for i in range(0,Nx2):
+    X2[i] = X2_min + i*h2*(X2_max-X2_min)
+    u2[i] = random.random()#u2_ini(X2[i])
+    
+for i in range(0,Nx3):
+    X3[i] = X3_min + i*h3*(X3_max-X3_min)
+    u3[i] = random.random()#u3_ini(X3[i])
+# pour la visu:
+#Y_min = 0
+#Y_max = 1.1*max(v_max,u_max) 
+
+# assembly of a sparse matrix M using the vector W of the previous time
+
+#Wn=numpy.hstack((u1,u2))
+#Wn=numpy.hstack((Wn,u3))
+Wn=numpy.hstack((u1,PH0))
+next_Wn = numpy.zeros(2*Nx1)
+
+def constr_matrix_Ai(ielem,const):
+    assert implicite
+    row = list()
+    col = list()
+    data = list()
+    coef=const
+    
+    row.append((0))
+    col.append((0))  
+    data.append(0)   # value of the element
+    
+    for i in range(1,Nx_array[ielem]-1):
+        # M_i,i = 0
+    
+        # M_i,i-1
+        j = i-1#numpy.mod(i-1,Nx_array[ielem])
+        row.append((i))
+        col.append((j))  
+        data.append( coef*1./2*dt_array[ielem] )   # value of the element
+
+        # M_i,i+1
+        j = i+1#numpy.mod(i+1,Nx_array[ielem])
+        row.append((i))
+        col.append((j)) 
+        data.append( -coef*1./2*dt_array[ielem] )  # value of the element  
+        
+    row.append((Nx_array[ielem]-1))
+    col.append((Nx_array[ielem]-1)) 
+    data.append(0)  # value of the element
+    row = numpy.array(row)
+    col = numpy.array(col)
+    data = numpy.array(data)      
+    M = (sparse.coo_matrix((data, (row, col)), shape=(Nx_array[ielem], Nx_array[ielem]))).tocsr()
+    return M
+
+M=constr_matrix_Ai(0,-Const_c);
+pprint(SparseMatrix(M.todense()))
+print("detM=",numpy.linalg.det(M.todense()))
+pprint(Wn)
+
+def assemble_M():
+    assert implicite
+    A1=constr_matrix_Ai(0,-Const_c)
+    A1_d=constr_matrix_Ai(0,-Const_d)
+    
+    I=sparse.identity(Nx1);
+    I=sparse.coo_matrix(I)
+
+    
+    #pprint(Zero)
+    #pprint(Zero.todense())
+    #pprint(sparse.coo_matrix(I))
+    #pprint((I))
+    B1=constr_matrix_Ai(0,Const_c)
+    B1_d=constr_matrix_Ai(0,Const_d)
+    
+    #pprint(SparseMatrix(A1.todense()))
+    #pprint(SparseMatrix(B1.todense()))
+    
+    M1=numpy.hstack((I.todense(),A1.todense()))
+    
+    M2=numpy.hstack((A1_d.todense(),I.todense()))
+    
+    M=numpy.vstack((M1,M2))
+    M=sparse.coo_matrix(M)
+    pprint(M)
+    
+    M1=numpy.hstack((I.todense(),B1.todense()))
+    
+    M2=numpy.hstack((B1_d.todense(),I.todense()))
+    
+    MB=numpy.vstack((M1,M2))
+    MB=sparse.coo_matrix(MB)
+    #pprint(MB)
+    #print(numpy.linalg.det(M.todense()))
+    #print(numpy.linalg.det(MB.todense()))
+    pprint(SparseMatrix(M.todense()))
+    #pprint(SparseMatrix(MB.todense()))
+    return M,MB
+
+#if do_movie:
+    #ims = []
+    #print("using imagemagick...")
+    #Writer = animation.writers['imagemagick']  # ['ffmpeg']
+    #writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+#fig = plt.figure()
+
+
+#def plot_sol(n,ielem):
+    #fig.clf()
+    #fname = dir_name+"out_"+repr(n)+"_u"+str(ielem)+".png"
+    #print("Plot sol in file ", fname, ", nt = ", n, ", min/max = ", min(u_array[ielem]), "/", max(u_array[ielem]))
+    #X_full = numpy.concatenate((X_array[ielem][2],[1.]),axis=0)
+    #u_full = numpy.concatenate((u_array[ielem],[u_array[ielem][0]]),axis=0)
+    ## trace aussi les vitesses:
+    ##v = map(vitesse, u)
+    ##v_full = numpy.concatenate((v,[v[0]]),axis=0)
+    #plt.xlim(X_array[ielem][0], X_array[ielem][1])
+    ##plt.ylim(Y_min, Y_max)
+    #plt.xlabel('x')
+    #image = (plt.plot(X_full, u_full, '-', color='k'))
+    ##maximage += (plt.plot(X_full, v_full, '-', color='b'))
+    
+    #fig.savefig(fname)
+    #if do_movie:
+        #ims.append(image)
+
+#plot_sol(0,0)
+#print(Wn)
+## schema numerique       
+#for nt in range(0,Nt):               
+    #if implicite:
+        #print("Implicit Scheme")
+        #M,MB = assemble_M()
+        #next_Wn[:] = sparse.linalg.spsolve(M, MB*Wn)
+
+    #else:
+        #print("ERROR")
+        #exit()
+        ##for i in range(0,Nx1):        
+            ##next_u[i] =-dt_over_h*(flux(u[i])-flux(u[i-1]))+u[i]   # ecrire ici le schema explicite
+            
+    #Nx=Nx1
+    #Wn[:] = next_Wn[:]
+    #u1[:] = next_Wn[0:Nx]
+    #u2[:] = next_Wn[Nx:2*Nx]
+    #u3[:] = next_Wn[2*Nx:3*Nx]
+    #PH0[:] = next_Wn[3*Nx:]
+    #u_array=[u1,u2,u3,PH0]
+    ##if (nt+1)%periode_images == 0 or (nt+1) == Nt:
+    #plot_sol(nt+1,0)
+    
+
+#if do_movie:
+    #im_ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=3000, blit=True)
+    #im_ani.save(movie_filename, writer=writer)
+
+M,MB=assemble_M()
+pprint(MB*Wn)
+print("Done.")
